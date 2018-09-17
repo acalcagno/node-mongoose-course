413dc258c4ae2e69cf9b67358f56982b7b21e8bf
acalcagno <calqui@gmail.com>
2018-09-16 22:24:32 -0300

    first Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{
+            new: true
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 4,
+            comment: 'I\'m getting a sinking feeling2!',
+            author: 'Leonardo di Carpaccio2'
+        });
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+        return db.collection('dishes').drop();
+    })
+    .then(() => {
+        return db.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
